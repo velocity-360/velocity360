25f8c198f4b1aaa7735557a49c573fdce2c6b927
fullstack360 <info@fullstack360.com>
2017-04-07 14:02:23 -0400

    comments

--- src/components/presentation/Comments.js
+++ src/components/presentation/Comments.js
@@ -3,15 +3,18 @@ import { TextUtils } from '../../utils'
 
 export default (props) => {
 
+	let image = null
+	if (props.image.length > 0)
+		image = (props.image.indexOf('http') == -1) ? 'https://media-service.appspot.com/site/images/'+props.image+'?crop=96' : props.image+'=s96-c'
+
 	return (
 
 		<div className="single-post nobottommargin">
 			<div className="entry clearfix" style={{border:'none',paddingBottom:0,marginBottom:0}}>
 				<div className="entry-title">
-					<h1>Comments: {props.title || props.name}</h1>
+					<h1>{props.title || props.name}</h1>
 				</div>
 				<hr />
-
 				<textarea onChange={props.onChangeComment.bind(this, 'text')} style={{border:'1px solid #ddd', height:80, width:100+'%', marginBottom:6, resize:'none', padding:6, borderRadius:2}}></textarea>
 				<div style={{width:100+'%', textAlign:'right', marginBottom:16}}>
 					<button onClick={props.onSubmit.bind(this)} className="button button-mini button-circle button-red">Submit</button>
