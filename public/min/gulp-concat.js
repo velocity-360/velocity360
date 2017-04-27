29440767c12e9a2162ece67c819776cad4f35480
fullstack360 <info@fullstack360.com>
2017-04-27 19:23:12 -0400

    redirect to turbo when loggin in

--- public/min/gulp-concat.js
+++ public/min/gulp-concat.js
@@ -4500,6 +4500,18 @@ var updateVisitor = function(field, event){
 	visitor[field] = event.target.value
 }
 
+var getParameterByName = function (name, url) {
+    if (!url) url = window.location.href
+
+    name = name.replace(/[\[\]]/g, "\\$&")
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+
+    if (!results) return null
+    if (!results[2]) return ''
+    return decodeURIComponent(results[2].replace(/\+/g, " "))
+}
+
 var login = function(event){
 	event.preventDefault()
 	if (visitor.email.length == 0){
@@ -4518,12 +4530,20 @@ var login = function(event){
         data: JSON.stringify(visitor),
         contentType: 'application/json; charset=utf-8',
         success: function(response) {
-        	if (response.confirmation == 'success'){
-        		window.location.href = '/account'
-        		return
+        	if (response.confirmation != 'success'){
+				alert(response.message)
+				return
         	}
 
-			alert(response.message)
+    		var redirect = getParameterByName('redirect')
+    		if (redirect == null){
+        		window.location.href = '/account'
+    			return
+    		}
+
+    		if (redirect == 'turbo')
+        		window.location.href = 'http://turbo.velocity360.io/'
+
         }
     })
 }
